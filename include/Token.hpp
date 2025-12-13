6fa519f970564c16375e3779bcb59731442d11ed
mayedl10 <danielmayer0708@gmail.com>
2025-12-13 20:04:18 +0100

    token struct and token type enum

--- include/Token.hpp
+++ include/Token.hpp
@@ -2,7 +2,25 @@
 #define TOKEN_HPP
 
 #include <cstdint>
+#include <string>
 
+enum TokenType : uint8_t {
+    TT_invalid,
+    TT_EOF,
+    TT_identifier,      // x, ?var (pointer prefix is part of name)
+    TT_instruction,     // if, set, ++, !=, etc.
+    TT_section,         // denoted by .
+    TT_subsection,      // denoted by :
+    TT_number,          // hexadecimal numbers
 
+};
+
+struct Token {
+    std::string content;    // exact eg. identifier this token contains
+    TokenType type;         // what kind of token is this?
+    
+    std::string file;       // used for error tracking
+    int lineInFile;         // line# in file for error tracking
+};
 
 #endif
